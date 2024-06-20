71ddc61fde294d8ff21caca0e435a5161a3f2f96
LautaroFuente <lautaro.fuente@yahoo.com>
2024-06-19 21:40:35 -0300

    update

--- frontend/src/components/RegisterClient.jsx
+++ frontend/src/components/RegisterClient.jsx
@@ -1,23 +1,86 @@
+import { useForm } from "../hooks/useForm";
+
 function RegisterClient() {
+  const initialForm = {
+    name: "",
+    last_name: "",
+    dni: "",
+    age: "",
+  };
+
+  const { form, handleInputChange, resetForm } = useForm(initialForm);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/server/clients/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error al agregar");
+      }
+
+      const data = await response.json();
+      console.log("Agregado con exito:", data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+
+    resetForm;
+  };
+
   return (
     <>
       <h1>¡REGISTRATE!</h1>
-      <form onSubmit={{}}>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Nombre:</label>
-          <input type="text" id="name" name="name" required />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={form.name}
+            onChange={handleInputChange}
+            required
+          />
         </div>
         <div>
-          <label htmlFor="lastName">Apellido:</label>
-          <input type="text" id="lastName" name="lastName" required />
+          <label htmlFor="last_name">Apellido:</label>
+          <input
+            type="text"
+            id="last_name"
+            name="last_name"
+            value={form.last_name}
+            onChange={handleInputChange}
+            required
+          />
         </div>
         <div>
           <label htmlFor="dni">DNI:</label>
-          <input type="text" id="dni" name="dni" required />
+          <input
+            type="text"
+            id="dni"
+            name="dni"
+            value={form.dni}
+            onChange={handleInputChange}
+            required
+          />
         </div>
         <div>
           <label htmlFor="age">Edad:</label>
-          <input type="number" id="age" name="age" required />
+          <input
+            type="number"
+            id="age"
+            name="age"
+            value={form.age}
+            onChange={handleInputChange}
+            required
+          />
         </div>
         <button type="submit" className="submit">
           Registrarse
---
+++ frontend/src/hooks/useForm.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export const useForm = (initialForm) => {
+  const [form, setForm] = useState(initialForm);
+
+  const handleInputChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const resetForm = () => {
+    setForm(initialForm);
+  };
+
+  return {
+    form,
+    handleInputChange,
+    resetForm,
+  };
+};
